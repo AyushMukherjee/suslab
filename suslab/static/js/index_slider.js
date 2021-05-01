1a384f461a3854acb87edd24644f5d5024fab2a6
ayush <ayushm03@gmail.com>
2021-05-01 12:26:08 +0530

    add arrow nav and increase section height

--- suslab/static/js/index_slider.js
+++ suslab/static/js/index_slider.js
@@ -4,8 +4,10 @@ function delay(ms) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    const slideList = document.querySelectorAll('.app-slide-content');
-	const appButtonlist = document.querySelectorAll('.app-button');
+  const slideList = document.querySelectorAll('.app-slide-content');
+  const appButtonlist = document.querySelectorAll('.app-button');
+  const prevButton = document.querySelector('.app-slide-nav-before');
+  const nextButton = document.querySelector('.app-slide-nav-after');
 
 	// init slider
 	let activeKey = 0;
@@ -13,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	// change slide
 	async function onSlideChange(key) {
+    key = (key % slideList.length + slideList.length) % slideList.length;
 	  if (key === activeKey) return;
 	  slideList[activeKey].classList.add('leave');
 	  await delay(400);
@@ -20,8 +23,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	  activeKey = key;
 	  slideList[key].classList.add('active');
 	}
-	
+  
+  // add button function
 	appButtonlist.forEach((button, key) => {
 	  button.addEventListener('click', () => onSlideChange(key))
-	})
+  })
+  
+  // add arrow function
+  prevButton.addEventListener('click', () => onSlideChange(activeKey - 1));
+  nextButton.addEventListener('click', () => onSlideChange(activeKey + 1));
 });
