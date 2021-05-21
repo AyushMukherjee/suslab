6d6310d89a544f3aacd99175fd30ac0c7e2a83cc
Prajwal Kishore <prajnk202@gmail.com>
2021-05-21 20:38:26 +0530

    Removed commented code, modified conditional view of index.html

--- suslab/__init__.py
+++ suslab/__init__.py
@@ -16,22 +16,6 @@ security = Security(app, get_user_datastore())
 
 csrf = CSRFProtect(app)
 
-'''
-get_user_datastore().find_or_create_role('admin')
-
-
-#creating admin-level user
-adminUser = get_user_datastore().create_user(
-    id = 999, 
-    email = 'admin@example.com',
-    password = 'password',
-    name = 'Admin',
-    active = True,
-    roles = ['admin']
-)
-'''
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
