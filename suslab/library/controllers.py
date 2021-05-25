e7b7619a5cb264f10cfb1adda2d39c4e58582de5
Prajwal Kishore <prajnk202@gmail.com>
2021-05-26 01:17:33 +0530

    removed admin_dummy.html, /admin route; formatted by PEP-8

--- suslab/library/controllers.py
+++ suslab/library/controllers.py
@@ -1,6 +1,5 @@
 from flask import request, render_template, Blueprint, url_for, redirect
 from flask_security import current_user
-from flask_security.decorators import roles_required
 from suslab.library.forms import ProductForm
 
 
@@ -48,10 +47,3 @@ def delete(id):
         return redirect(url_for('library.index'))
     except:
         return 'There was a problem deleting that task'
-
-@library.route('/admin')
-#@roles_required('admin') # only admins can access
-def go_to_admin():
-    Product, db = _db_conn()
-    items = Product.query.order_by(Product.date_created).all()
-    return render_template('library/admin_dummy.html', items=items)
