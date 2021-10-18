791e3a9150d4359f82a7107975a7bc79c216b51d
Ayush Mukherjee <ayushm03@gmail.com>
2021-10-18 14:46:54 +0530

    add minor transitions

--- suslab/static/js/nav.js
+++ suslab/static/js/nav.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
 
     navToggle.addEventListener('click', () => {
-    document.querySelector('nav').classList.toggle('nav-open');
+        document.querySelector('nav').classList.toggle('nav-open');
+        document.querySelector('.window-top>header').classList.toggle('hide');
     });
 });
