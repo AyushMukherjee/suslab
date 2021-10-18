2be362ac97156f022e88c2997e5ed45545f1ac62
Ayush Mukherjee <ayushm03@gmail.com>
2021-10-18 22:14:25 +0530

    add username and profile

--- suslab/__init__.py
+++ suslab/__init__.py
@@ -6,6 +6,7 @@ from flask_wtf.csrf import CSRFProtect
 from suslab.library.controllers import library
 from suslab.pool.controllers import pool
 from suslab.users.controllers import get_user_datastore
+from suslab.users.forms import ExtendedRegisterForm
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -15,7 +16,7 @@ app.register_blueprint(library)
 app.register_blueprint(pool)
 
 db = SQLAlchemy(app)
-security = Security(app, get_user_datastore())
+security = Security(app, get_user_datastore(), register_form=ExtendedRegisterForm)
 
 csrf = CSRFProtect(app)
 
@@ -28,6 +29,14 @@ def index():
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+
+@app.context_processor
+def utility_processor():
+    def user_avatar(user_name, category='croodles-neutral'):
+        user_name_clean = '-'.join(user_name.split())
+        return f'https://avatars.dicebear.com/api/{category}/{user_name_clean}.svg'
+    return dict(user_avatar=user_avatar)
 
 
 def get_app():
