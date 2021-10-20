dd4a1d02928fd793b5cd23a45121d9ffe1c5137c
Ayush Mukherjee <ayushm03@gmail.com>
2021-10-21 03:34:10 +0530

    update nav toggle triggers

--- suslab/static/js/nav.js
+++ suslab/static/js/nav.js
@@ -1,10 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
-    const navbar = document.querySelector('.navbar')
-
-    navToggle.addEventListener('click', () => {
-        document.querySelector('nav').classList.toggle('nav-open');
+    const nav = document.querySelector('nav');
+    
+    const toggleNav = () => {
+        nav.classList.toggle('nav-open');
         document.querySelector('.window-top>header').classList.toggle('hide');
         document.querySelector('.next-section').classList.toggle('hide');
+    };
+
+    navToggle.addEventListener('click', e => {
+        e.stopPropagation();
+        toggleNav();
     });
+
+    nav.addEventListener('click', e => {
+        e.stopPropagation();
+    });
+
+    document.querySelector('body').addEventListener('click', () => {
+        if(nav.classList.contains('nav-open')) {
+            toggleNav();
+        }
+     });
 });
