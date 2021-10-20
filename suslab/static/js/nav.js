77e88645bb4b77d13023558234690c9be61d7646
Ayush Mukherjee <ayushm03@gmail.com>
2021-10-21 03:14:03 +0530

    change navbar opacity on scrolling

--- suslab/static/js/nav.js
+++ suslab/static/js/nav.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
+    const navbar = document.querySelector('.navbar')
 
     navToggle.addEventListener('click', () => {
         document.querySelector('nav').classList.toggle('nav-open');
