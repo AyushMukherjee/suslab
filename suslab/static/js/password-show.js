b1ff0e11313b0a063dababaa10f9273d1a0b1de2
Asma Memon <memonasma73@gmail.com>
2021-10-22 14:39:36 +0530

    minor bugfix

--- suslab/static/js/password-show.js
+++ suslab/static/js/password-show.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
   const togglePassword = document.querySelector('#togglePassword');
   const password = document.querySelector('#password');
   
-  togglePassword.addEventListener('click', e => {
+  togglePassword.addEventListener('click', () => {
     type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
-    console.log(this);
     togglePassword.classList.toggle('fa-eye-slash');
   });
-});
+  });
+
+  
