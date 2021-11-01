c52c4ef947f91e5dba7b045bf00b4e1b222cee10
Ayush Mukherjee <ayushm03@gmail.com>
2021-11-01 13:37:26 +0530

    refactor js socket into separate files

--- suslab/users/forms.py
+++ suslab/users/forms.py
@@ -1,6 +1,9 @@
+'This module extends the security forms from flask-security'
+
 from flask_security import RegisterForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 class ExtendedRegisterForm(RegisterForm):
+    'Extends the registration form to ask for user name'
     name = StringField('Name', [DataRequired()])
